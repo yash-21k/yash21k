f407cd2e65b9677de477f25fa0be3cadbb767039
username <wecp-dummy@gmail.com>
2024-02-01 18:24:47 +0000

    Day 10

--- backend/src/main/java/com/wecp/progressive/repository/LoanRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/LoanRepository.java
@@ -1,6 +1,10 @@
 package com.wecp.progressive.repository;
 
 
+import com.wecp.progressive.entity.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface LoanRepository {
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
 }
