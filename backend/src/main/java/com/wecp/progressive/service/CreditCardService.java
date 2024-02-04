2399be6673e8a650234fefbb30359763e6a7f1ab
username <wecp-dummy@gmail.com>
2024-02-04 18:30:57 +0000

    Day 11 commit

--- backend/src/main/java/com/wecp/progressive/service/CreditCardService.java
+++ backend/src/main/java/com/wecp/progressive/service/CreditCardService.java
@@ -2,28 +2,38 @@ package com.wecp.progressive.service;
 
 
 import com.wecp.progressive.entity.CreditCard;
+import com.wecp.progressive.repository.CreditCardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CreditCardService {
+    private final CreditCardRepository creditCardRepository;
+
+    @Autowired
+    public CreditCardService(CreditCardRepository creditCardRepository) {
+        this.creditCardRepository = creditCardRepository;
+    }
 
     public List<CreditCard> getAllCreditCards() {
-        return null;
+        return creditCardRepository.findAll();
     }
 
     public CreditCard getCreditCardById(Long id) {
-        return null;
+        return creditCardRepository.findById(id).orElse(null);
     }
 
     public CreditCard createCreditCard(CreditCard creditCard) {
-        return null;
+        return creditCardRepository.save(creditCard);
     }
 
     public void updateCreditCard(CreditCard creditCard) {
-
+        creditCardRepository.save(creditCard);
     }
 
     public void deleteCreditCard(Long id) {
-
+        creditCardRepository.deleteById(id);
     }
 }
